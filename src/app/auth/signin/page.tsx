29cbe004851e3bf28201a0c1fb867f4ce0b40661
jsstoni <jsstoniha@gmail.com>
2024-05-07 18:45:21 -0400

    action login

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,15 +1,41 @@
+"use client";
+
 import { Button, Input } from "@/components";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Signin() {
+  const router = useRouter();
+
+  async function login(formData: FormData) {
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+    if (res && res.error) {
+      alert(res.error);
+    } else {
+      router.push("/");
+    }
+  }
+
   return (
-    <form action="" className="flex flex-col w-full">
+    <form action={login} className="flex flex-col w-full">
       <label htmlFor="">Email</label>
-      <Input type="text" />
+      <Input type="text" name="email" />
 
       <label htmlFor="">Password</label>
-      <Input type="password" />
+      <Input type="password" name="password" />
 
-      <Button>Login</Button>
+      <Button type="submit" className="mt-3">
+        Login
+      </Button>
+
+      <Link href="/auth/signup" className="mt-3">
+        I need an account !
+      </Link>
     </form>
   );
 }
