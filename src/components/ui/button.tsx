4e7c41d6e7583d7e496eda0fde0481147ca3946b
jsstoni <jsstoniha@gmail.com>
2024-05-07 15:04:14 -0400

    variant secondary

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,14 +3,15 @@ import React from "react";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
-  variant: "default" | "outline" | "link" | "ghost";
+  variant?: "default" | "secondary" | "outline" | "link" | "ghost";
 }
 
 const Button = React.forwardRef<HTMLButtonElement, Props>(
-  ({ variant = "default", children, ...props }, ref) => {
+  ({ variant, className, children, ...props }, ref) => {
     //class variant
     const cv = {
       default: "bg-black text-white dark:bg-white dark:text-black",
+      secondary: "bg-blue-600 text-white",
       outline: "ring-1 ring-zinc-200 dark:ring-zinc-800 hover:dark:bg-zinc-900",
       ghost: "hover:bg-zinc-200 hover:dark:bg-zinc-900",
       link: "text-blue-500 hover:text-blue-800 hover:underline text-left",
@@ -18,7 +19,7 @@ const Button = React.forwardRef<HTMLButtonElement, Props>(
 
     return (
       <button
-        className={cn("p-1.5 rounded-md", cv[variant])}
+        className={cn("p-1.5 rounded-md", cv[variant || "default"], className)}
         ref={ref}
         {...props}
       >
