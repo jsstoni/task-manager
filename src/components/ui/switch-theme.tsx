dd9011c06744ed20dd3785299c27a8205064607f
jsstoni <jsstoniha@gmail.com>
2024-05-09 06:08:48 -0400

    fix

--- src/components/ui/switch-theme.tsx
+++ src/components/ui/switch-theme.tsx
@@ -1,10 +1,21 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
 export function SwitchTheme() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return theme === "dark" ? (
     <BsSun onClick={() => setTheme("light")} />
   ) : (
