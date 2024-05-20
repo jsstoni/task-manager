dc2e1716a002fc1c96befa07746ecb390a4d7cbb
jsstoni <jsstoniha@gmail.com>
2024-05-19 20:06:13 -0400

    bg border colors

--- src/components/board/items-cards.tsx
+++ src/components/board/items-cards.tsx
@@ -34,7 +34,7 @@ export function ItemsCards({ task }: Props) {
     <article
       className={cn(
         { "opacity-10": idTask === task.id },
-        "flex flex-col items-start gap-1 rounded-md border p-3 dark:border-zinc-900 dark:bg-zinc-900/60 hover:dark:border-slate-800",
+        "flex flex-col items-start gap-1 rounded-md border p-3.5 shadow dark:border-zinc-900 dark:bg-zinc-900 hover:dark:border-slate-800",
       )}
       draggable
       onDragStart={(ev) => onDragStart(ev, task.id)}
@@ -49,7 +49,7 @@ export function ItemsCards({ task }: Props) {
       <div className="flex w-full items-center gap-2 text-sm text-zinc-500">
         <BadgePriority value={task.priority} />
 
-        <RowItems className="rounded-md bg-zinc-950/30 px-2 py-0.5">
+        <RowItems className="rounded-md bg-zinc-100 px-2 py-0.5 dark:bg-zinc-950/30">
           <BsCalendarDate /> {duedate}
         </RowItems>
 
