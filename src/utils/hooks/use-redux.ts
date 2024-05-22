31419aecd871b7888d6e586688c705807c53a99d
jsstoni <jsstoniha@gmail.com>
2024-05-22 00:08:17 -0400

    withTypes

--- src/utils/hooks/use-redux.ts
+++ src/utils/hooks/use-redux.ts
@@ -1,5 +1,5 @@
-import { AppDispatch, RootState } from "@/utils/libs/redux";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../libs/redux";
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
