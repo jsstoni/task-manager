086a01c7793e2b2dba4b85c6ed41a3555ccfdfaf
jsstoni <jsstoniha@gmail.com>
2024-05-23 14:19:57 -0400

    save time in localstorage

--- src/components/ui/time-tracking.tsx
+++ src/components/ui/time-tracking.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Select } from "@/components";
 import { BsPauseFill, BsPlayFill, BsStopFill } from "react-icons/bs";
 import { useBoard } from "@/utils/hooks";
 
+interface TimerData {
+  timer: number;
+  status: "play" | "pause";
+  id: number;
+  lastUpdate: number;
+}
+
 export function TimeTracking() {
   const [counter, setCounter] = useState<number>(0);
   const [start, setStart] = useState<boolean>(false);
-  const [isTaskId, setTaskId] = useState<number | null>(null);
+  const [taskId, setTaskId] = useState<number | null>(null);
 
   const { tasks } = useBoard();
 
@@ -16,6 +23,21 @@ export function TimeTracking() {
     () => tasks.filter((task) => task.column === "In Progress"),
     [tasks],
   );
+
+  useEffect(() => {
+    const savedData = window.localStorage.getItem("timer");
+    if (savedData) {
+      const { timer, status, id, lastUpdate }: TimerData =
+        JSON.parse(savedData);
+      setTaskId(id);
+      setStart(status === "play");
+      setCounter(
+        status === "play"
+          ? timer + Math.floor((Date.now() - lastUpdate) / 1000)
+          : timer,
+      );
+    }
+  }, []);
 
   useEffect(() => {
     if (!start) return;
@@ -27,19 +49,43 @@ export function TimeTracking() {
     return () => clearInterval(interval);
   }, [start]);
 
-  const handleStart = () => {
-    if (isTaskId) {
-      setStart((prevStart) => !prevStart);
-      return;
-    }
-    alert("choose yout task");
+  const setLocalData = (
+    timer: number,
+    id: number,
+    status: "play" | "pause",
+  ) => {
+    const localData: TimerData = {
+      timer,
+      status,
+      id,
+      lastUpdate: Date.now(),
+    };
+    window.localStorage.setItem("timer", JSON.stringify(localData));
   };
 
-  const handleStop = () => {
+  const handleStart = useCallback(() => {
+    if (taskId) {
+      setLocalData(counter, taskId, "play");
+      setStart(true);
+      return null;
+    }
+    alert("choose yout task");
+  }, [counter, taskId]);
+
+  const handlePause = useCallback(() => {
+    const localTimer = window.localStorage.getItem("timer");
+    if (localTimer && taskId) {
+      setLocalData(counter, taskId, "pause");
+      setStart(false);
+    }
+  }, [counter, taskId]);
+
+  const handleStop = useCallback(() => {
     setCounter(0);
     setStart(false);
     setTaskId(null);
-  };
+    window.localStorage.removeItem("timer");
+  }, []);
 
   const formatTime = (seconds: number): string => {
     const hrs = Math.floor(seconds / 3600);
@@ -53,6 +99,7 @@ export function TimeTracking() {
       <Select
         name="taskid"
         className="mr-2 max-w-[160px] px-2 py-1"
+        value={taskId ?? 0}
         onChange={(e) => setTaskId(+e.target.value)}
       >
         <option>choose your task</option>
@@ -64,7 +111,7 @@ export function TimeTracking() {
       </Select>
 
       <button
-        onClick={handleStart}
+        onClick={!start ? handleStart : handlePause}
         className="rounded-full bg-gray-100 p-1 ring-1 ring-gray-400 dark:bg-zinc-900 dark:ring-zinc-600"
       >
         {start ? <BsPauseFill size={22} /> : <BsPlayFill size={22} />}
