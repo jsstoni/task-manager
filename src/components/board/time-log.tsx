fa95f76b8249188470a2369235b67806c553eb31
jsstoni <jsstoniha@gmail.com>
2024-05-25 01:09:08 -0400

    color text

--- src/components/board/time-log.tsx
+++ src/components/board/time-log.tsx
@@ -11,7 +11,7 @@ export function TimeLog({ value }: Props) {
 
   return (
     value > 0 && (
-      <RowItems className="rounded-md bg-zinc-950/30 px-2 py-0.5 text-xs">
+      <RowItems className="rounded-md bg-zinc-950/30 px-2 py-0.5 text-xs text-zinc-500">
         <BsClock /> Log: {log}
       </RowItems>
     )
