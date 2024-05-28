8f42c25966e80021dd9cdf43bd990ef6d983aea2
jsstoni <jsstoniha@gmail.com>
2024-05-28 17:55:56 -0400

    prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,7 @@
-/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
 /** @typedef  {import("prettier").Config} PrettierConfig*/
 /** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
 
-/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+/** @type { PrettierConfig | TailwindConfig } */
 const config = {
   arrowParens: "always",
   printWidth: 80,
